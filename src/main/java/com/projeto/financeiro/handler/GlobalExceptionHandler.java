ce7c4dc57334c59c6a643da94c34cc8646db41c4
Marcelo <marcelopinotti@protonmail.com>
2026-04-10 14:04:44 -0300

    feat: Implement global exception handler for standardized error responses

---
+++ src/main/java/com/projeto/financeiro/handler/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.projeto.financeiro.handler;
+
+import com.projeto.financeiro.exception.ApiError;
+import com.projeto.financeiro.exception.BadRequestException;
+import com.projeto.financeiro.exception.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiError> handleRecursoNaoEncontrado(NotFoundException ex, HttpServletRequest request) {
+        ApiError error = new ApiError(
+                LocalDateTime.now().format(formatter),
+                HttpStatus.NOT_FOUND.value(),
+                "Recurso não encontrado",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiError> handleNegocioException(BadRequestException ex, HttpServletRequest request) {
+        ApiError error = new ApiError(
+                LocalDateTime.now().format(formatter),
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de negócio",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    // Para fins de log no console
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGenericException(Exception ex, HttpServletRequest request) {
+        ex.printStackTrace(); //
+        ApiError error = new ApiError(
+                LocalDateTime.now().format(formatter),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno do servidor",
+                "Ocorreu um erro inesperado no sistema. Tente novamente mais tarde.",
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
