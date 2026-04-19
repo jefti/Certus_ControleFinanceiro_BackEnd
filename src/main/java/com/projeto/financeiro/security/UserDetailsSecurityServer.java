c8b2fcb5adae285338d0b1bb819587f038444ab9
Marcelo <marcelopinotti@protonmail.com>
2026-04-18 22:24:53 -0300

    feat(security): implement UserDetailsService for user authentication using email

---
+++ src/main/java/com/projeto/financeiro/security/UserDetailsSecurityServer.java
@@ -0,0 +1,21 @@
+package com.projeto.financeiro.security;
+
+import com.projeto.financeiro.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsSecurityServer implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário ou Senha inválidos"));
+    }
+}
