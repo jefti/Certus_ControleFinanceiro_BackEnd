507d1372dfdba7d38704a9227514d8c0ab7e974d
jefti <jeftimeira@gmail.com>
2026-04-21 00:54:44 -0300

    docs: add bearer auth to swagger for user endpoints

---
+++ src/main/java/com/projeto/financeiro/security/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.projeto.financeiro.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Certus Controle Financeiro API",
+                version = "v1",
+                description = "Documentacao da API de controle financeiro"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
