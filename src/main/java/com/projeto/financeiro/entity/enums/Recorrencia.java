129a397367c33faac224b99d4cb36ff33d9e5799
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-05-21 09:54:45 -0300

    feat(enum): add Recorrencia and StatusTitulo enums for financial status management

---
+++ src/main/java/com/projeto/financeiro/entity/enums/Recorrencia.java
@@ -0,0 +1,20 @@
+package com.projeto.financeiro.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Recorrencia {
+    MENSAL("Mensal"),
+    BIMESTRAL("Bimestral"),
+    TRIMESTRAL("Trimestral"),
+    SEMESTRAL("Semestral"),
+    ANUAL("Anual");
+
+
+    private final String descricao;
+
+    Recorrencia(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
